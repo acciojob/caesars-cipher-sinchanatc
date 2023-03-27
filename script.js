1ba29b76154ad0cfdd586f09783e097567608f83
sinchanatc <92010565+sinchanatc@users.noreply.github.com>
2023-03-27 21:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function rot13(encodedStr){
 	}
 		decword.push(decodedArr)
 	}
-  return ;//return decodedArr
+  return decodedArr.join(" ") ;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
